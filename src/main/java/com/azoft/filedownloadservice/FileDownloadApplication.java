9f9206f22e9f0dc4e1fdb5634a8fafc754b938b7
baymxs.n@gmail.com <BBoringPassWord990710G>
2020-12-03 14:44:20 +0700

    Add war generating

--- src/main/java/com/azoft/filedownloadservice/FileDownloadApplication.java
+++ src/main/java/com/azoft/filedownloadservice/FileDownloadApplication.java
@@ -2,9 +2,16 @@ package com.azoft.filedownloadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FileDownloadApplication {
+public class FileDownloadApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FileDownloadApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FileDownloadApplication.class, args);
     }
